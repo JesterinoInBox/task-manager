248fb645c5cb601b8c408ad9f0c46915b5408e9b
JesterinoInBox <toliksankin@gmail.com>
2026-06-29 12:48:37 +0300

    Initial Commit with dependencies

---
+++ src/test/java/com/jesterino/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesterino.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
